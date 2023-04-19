a8b4a53befdab2a5224ad8b5c674a7ed005e15a8
Shakib Rahman <cakoshakib@gmail.com>
2023-04-19 18:17:15 -0400

    start implementing two approximation

--- novel/rlis.h
+++ novel/rlis.h
@@ -1,13 +1,24 @@
 #include <vector>
 #include <utility>
+#include <map>
+
+typedef std::map<std::pair<int,int>, int> query_map_t;
+typedef std::pair<int,int> query_t;
 
 class RLIS {
 public:
-    RLIS(std::vector<int> sequence, std::vector<std::pair<int,int>> queries);
+    RLIS(std::vector<int> sequence, std::vector<query_t> queries);
     std::vector<int> run();
 
 private:
+    // Variables
     std::vector<int> seq;
-    std::vector<std::pair<int,int>> queries;
+    std::vector<query_t> queries;
+
+    // Primary Algorithms
+    query_map_t two_approx();
+
+    // Helper Functions
+    void build_approx_tree(ApproxNode *node);
 
 };
--- novel/two_approx.cpp
+++ novel/two_approx.cpp
@@ -1,2 +1,38 @@
 #include "rlis.h"
+using namespace std;
 
+class ApproxNode {
+public:
+    int mid, start, end;
+    int data;
+    ApproxNode *left, *right;
+    ApproxNode(int start, int end) {
+        this->start = start;
+        this->end = end;
+        this->mid = mid_point(start, end);
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+
+    int mid_point(int x, int y) {
+        return x + ((y - x) / 2);
+    }
+
+};
+
+void RLIS::build_approx_tree(ApproxNode *node) {
+    return;
+}
+
+
+query_map_t RLIS::two_approx() {
+    vector<int> seq = this->seq;
+    int n = seq.size();
+
+    ApproxNode *root = new ApproxNode(0, n-1);
+    build_approx_tree(root);
+
+    query_map_t approx;
+
+    return approx;
+}
