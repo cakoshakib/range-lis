c40a0069c387e8844a4a3811c4db77199ee8b081
cakoshakib <cakoshakib@gmail.com>
2023-04-24 13:13:48 -0400

    standard LIS nlogn impl

--- novel/rlis.h
+++ novel/rlis.h
@@ -1,14 +1,41 @@
 #include <vector>
 #include <utility>
+#include <iostream>
 #include <map>
+#define INF 1e5
 
 typedef std::map<std::pair<int,int>, int> query_map_t;
 typedef std::pair<int,int> query_t;
+
+class ApproxNode {
+public:
+    int mid, start, end;
+    int data;
+    ApproxNode *left, *right;
+    std::vector<int> approx;
+    ApproxNode(int start, int end) {
+        this->start = start;
+        this->end = end;
+        this->mid = mid_point(start, end);
+        this->left = nullptr;
+        this->right = nullptr;
+        std::vector<int> approximation(end-start+1);
+        this->approx = approximation;
+    }
+
+    int mid_point(int x, int y) {
+        return x + ((y - x) / 2);
+    }
+
+};
 
 class RLIS {
 public:
     RLIS(std::vector<int> sequence, std::vector<query_t> queries);
     std::vector<int> run();
+    std::vector<int> forward_lis(int i, int j);
+    std::vector<int> backward_lis(int i, int j);
+    query_map_t two_approx();
 
 private:
     // Variables
@@ -16,9 +43,9 @@ private:
     std::vector<query_t> queries;
 
     // Primary Algorithms
-    query_map_t two_approx();
 
     // Helper Functions
     void build_approx_tree(ApproxNode *node);
+    int answer_query(ApproxNode *node, int i, int j);
 
 };
---
+++ novel/standard.cpp
@@ -0,0 +1,46 @@
+#include "rlis.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> RLIS::forward_lis(int lb, int ub) {
+    if (ub < lb) {
+        cout << "Invalid parameters to forward_lis, j > i\n";
+        exit(1);
+    }
+    vector<int> seq = this->seq;
+    int n = ub - lb + 1;
+    vector<int> d(n+1, INF);
+    d[0] = -INF;
+    vector<int> res(n, INF);
+    int longest = 0;
+    for (int i = lb; i <= ub; i++) {
+        int l = lower_bound(d.begin(), d.end(), seq[i-lb]) - d.begin();
+        d[l] = seq[i-lb];
+        longest = max(longest, l);
+        res[i - lb] = longest;
+    }
+    return res;
+}
+
+vector<int> RLIS::backward_lis(int lb, int ub) {
+    if (ub < lb) {
+        cout << "Invalid parameters to forward_lis, j > i\n";
+        exit(1);
+    }
+    vector<int> seq = this->seq;
+    int n = ub - lb + 1;
+    vector<int> d(n+1, -INF);
+    d[0] = INF;
+    vector<int> res(n, INF);
+    int longest = 0;
+    for (int i = ub; i >= lb; i--) {
+        int l = lower_bound(d.begin(), d.end(), seq[i-lb], greater<int>()) - d.begin();
+        d[l] = seq[i-lb];
+        longest = max(longest, l);
+        res[i - lb] = longest;
+    }
+    return res;
+
+}
